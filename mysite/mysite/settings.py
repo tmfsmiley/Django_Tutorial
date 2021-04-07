6168983d1593ee3e7cc9b9f8c08d2d5123ae3abc
Christine Noonan <cnoonan@fool.com>
2021-04-07 13:58:54 -0400

    set up files

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
-
+    'ratings.apps.RatingsConfig', 
 ]
 
 MIDDLEWARE = [
--- mysite/ratings/apps.py
+++ mysite/ratings/apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
 
-
-class PollsConfig(AppConfig):
+class RatingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'polls'
-
-
-
-
+    name = 'ratings'
